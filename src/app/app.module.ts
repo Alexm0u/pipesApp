021adac48a3fb3399f267c74417c90091438a4d0
Álex <alejandro.mcamps@bosonit.com>
2023-09-14 11:58:28 +0200

    FEAT: Added data in spanish by default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+// Config local de la App
+import localEs from '@angular/common/locales/es';
+
+import { registerLocaleData} from '@angular/common';
+
+registerLocaleData(localEs);
+
 
 @NgModule({
   declarations: [
@@ -18,7 +26,9 @@ import { SharedModule } from './shared/shared.module';
     
     
   ],
-  providers: [],
+  providers: [{
+    provide:LOCALE_ID, useValue: 'es'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
