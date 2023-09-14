779e351f8fb95d4764d789a01899517d3123ad6c
Álex <alejandro.mcamps@bosonit.com>
2023-09-14 13:43:30 +0200

    FEAT: Added i18n Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18nSelect
+  public name: string = 'Alex';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Carmen';
+    this.gender = 'female';
+  }
+
 }
