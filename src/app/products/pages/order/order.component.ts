684c516d995a073525382baf91b8cfaecd3ce916
Álex <alejandro.mcamps@bosonit.com>
2023-09-18 09:09:52 +0200

    FEAT: Added heroes table before custom pipe

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'products-order',
@@ -9,6 +10,34 @@ import { Component } from '@angular/core';
 export class OrderComponent {
 
   public isUpperCase: boolean = false;
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.black
+    },
+    {
+      name: 'Deadpool',
+      canFly: false,
+      color: Color.red
+    },
+    {
+      name: 'Hulk',
+      canFly: true,
+      color: Color.green
+    },
+    {
+      name: 'Doctor Strange',
+      canFly: true,
+      color: Color.blue
+    },
+]
 
   toggleUpperCase ():void {
     this.isUpperCase = !this.isUpperCase
